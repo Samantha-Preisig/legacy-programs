c0e8477e65cd6c8819ee337f91af7aa5bd9dbad6
Samantha Preisig <spreisig@uoguelph.ca>
2023-01-20 17:39:54 -0500

    Dictionary stored as dynamic array for each session

--- lexicon.f95
+++ lexicon.f95
@@ -1,4 +1,5 @@
 module lexicon
+    
     implicit none
 
     ! Types in Fortran are equivalent to structs in C
@@ -11,7 +12,36 @@ module lexicon
         type (raggedStr), dimension(:), allocatable :: list ! list is a deferred length "array" of type raggedStr
     end type strList
 
-    type(strList) :: jumbledWords, anagrams
-    integer, parameter :: maxSize=20
+    type(strList) :: jumbledWords
+    type(strList) :: dictionary
+    integer, parameter :: maxSize=20, maxLen=256
+    character(len=9), parameter :: filename = 'words.txt'
+    integer, parameter :: fileUnit = 1000
+
+    contains
+
+        ! function buildLexicon()
+        !     ! TODO
+        ! end function buildLexicon
+
+        integer function findNumLines(f_id) result(numLines)
+            integer :: i, eof
+            ! integer, intent(out) :: numLines
+            integer, intent(in) :: f_id
+            character(len=1) :: temp
+
+            rewind(f_id) ! rewind to the beginning of the file
+            
+            numLines = 0
+            do
+                read(f_id, '(A1)', iostat=eof) temp
+                if(eof < 0) exit ! end of file
+                numLines = numLines + 1
+            end do
+
+            rewind(f_id) ! rewind to the beginning of the file
+
+        end function findNumLines
+
 
 end module lexicon
--- solvejumble.f95
+++ solvejumble.f95
@@ -1,73 +1,94 @@
-program test
+! for lexicon.mod: gfortran -c lexicon.f95
+! gfortran -Wall solvejumble.f95 lexicon.f95
+
+program solvejumble
     
     use lexicon
     implicit none
 
     character, dimension(1) :: inputWord
+    integer :: lines, i
+    character(len=maxSize) :: lineInput
+
+    ! Open dictonary file (words.txt) and read/store words:
+    open(unit=fileUnit, file=filename)
+    lines = findNumLines(fileUnit)
+    allocate(dictionary%list(lines))
+    do i = 1, lines
+        read(fileUnit, '(A)') lineInput
+        allocate(character(len=len(trim(lineInput))) :: dictionary%list(i)%word)
+        dictionary%list(i)%word = trim(lineInput)
+    end do
+    close(fileUnit)
+
+    ! do i = 1, lines
+    !     print *, dictionary%list(i)%word
+    ! end do
+
     call inputJumble()
 
     contains
 
-    subroutine inputJumble()
+        subroutine inputJumble()
 
-        integer :: numWords, i, inputLen, j
-        character(len=maxSize) :: input
+            integer :: numWords, i, inputLen, j
+            character(len=maxSize) :: input
 
-        print *, "How many jumbled words?"
-        read (*,*) numWords
+            print *, "How many jumbled words?"
+            read (*,*) numWords
 
-        allocate(jumbledWords%list(numWords))
+            allocate(jumbledWords%list(numWords))
 
-        print *, "Enter the ", numWords, " jumbled words:"
-        do i = 1, numWords
-            print *, "> "
-            read (*,*) input
-            inputLen = len(trim(input))
-            allocate(character(len=inputLen) :: jumbledWords%list(i)%word)
-            jumbledWords%list(i)%word = trim(input)
+            print *, "Enter the ", numWords, " jumbled words:"
+            do i = 1, numWords
+                print *, "> "
+                read (*,*) input
+                inputLen = len(trim(input))
+                allocate(character(len=inputLen) :: jumbledWords%list(i)%word)
+                jumbledWords%list(i)%word = trim(input)
 
-            do j = 1, inputLen
-                inputWord(j) = achar(iachar(input(j:j)))
+                do j = 1, inputLen
+                    inputWord(j) = achar(iachar(input(j:j)))
+                end do
+
+                call generateAnagram(inputWord, 1, inputLen)
             end do
 
-            call generateAnagram(inputWord, 1, inputLen)
-        end do
+        end subroutine inputJumble
 
-    end subroutine inputJumble
+        recursive subroutine generateAnagram(word, left, right)
+            character, dimension(1), intent(inout) :: word
+            integer, intent(in) :: left, right
+            integer :: i
 
-    recursive subroutine generateAnagram(word, left, right)
-        character, dimension(1), intent(inout) :: word
-        integer, intent(in) :: left, right
-        integer :: i
+            if(left == right) then
+                print *, "anagram: ", word(1:right)
+            else
+                do i = left, right
+                    call swap(word, left, i)
+                    call generateAnagram(word, left+1, right)
+                    call swap(word, left, i)
+                end do
+            end if
 
-        if(left == right) then
-            print *, "anagram: ", word(1:right)
-        else
-            do i = left, right
-                call swap(word, left, i)
-                call generateAnagram(word, left+1, right)
-                call swap(word, left, i)
-            end do
-        end if
+        end subroutine generateAnagram
+        
+        ! word: the current input (jumbled) word to create anagrams from
+        ! wordLen: length of word
+        ! i, j: positions to swap
+        subroutine swap(word, i, j)
+            character, dimension(1), intent(inout) :: word
+            integer, intent(in) :: i, j
+            character :: temp
 
-    end subroutine generateAnagram
-    
-    ! word: the current input (jumbled) word to create anagrams from
-    ! wordLen: length of word
-    ! i, j: positions to swap
-    subroutine swap(word, i, j)
-        character, dimension(1), intent(inout) :: word
-        integer, intent(in) :: i, j
-        character :: temp
+            temp = word(i)
+            word(i) = word(j)
+            word(j) = temp
 
-        temp = word(i)
-        word(i) = word(j)
-        word(j) = temp
+        end subroutine swap
 
-    end subroutine swap
+        subroutine findAnagram()
+            ! TODO
+        end subroutine findAnagram
 
-    subroutine findAnagram()
-        ! TODO
-    end subroutine
-
-end program test
+end program solvejumble
